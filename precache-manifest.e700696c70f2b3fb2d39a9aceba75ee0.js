c93fe3b0fb15c7a50e6c48bcb247d1cfc861c812
Marco Baiad <marcobaiad@gmail.com>
2020-09-18 20:29:22 -0300

    Updates

---
+++ precache-manifest.e700696c70f2b3fb2d39a9aceba75ee0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f4494cf8bd70443432df369d65b329d",
+    "url": "/tramites/index.html"
+  },
+  {
+    "revision": "e9a262dd5556f82adcd7",
+    "url": "/tramites/static/css/main.5c836b57.chunk.css"
+  },
+  {
+    "revision": "45c6b96a278b2d7d725d",
+    "url": "/tramites/static/js/2.723fd4e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tramites/static/js/2.723fd4e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9a262dd5556f82adcd7",
+    "url": "/tramites/static/js/main.532895c9.chunk.js"
+  },
+  {
+    "revision": "d90ed03366098e9f05c4",
+    "url": "/tramites/static/js/runtime-main.3699a4e9.js"
+  },
+  {
+    "revision": "7b1a09e75312a6b1476fa549ff15fd24",
+    "url": "/tramites/static/media/headerImgresize.7b1a09e7.PNG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tramites/static/media/logo.5d5d9eef.svg"
+  }
+]);
